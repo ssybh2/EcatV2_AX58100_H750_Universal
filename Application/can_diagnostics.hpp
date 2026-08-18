14a780e6eb86dd12ca9f5ee03d701c639f25508c
Boyang Hu <ssybh2@nottingham.edu.cn>
2026-08-18 15:36:14 +0800

    expose atomic CAN diagnostic snapshots

--- Application/can_diagnostics.hpp
+++ Application/can_diagnostics.hpp
@@ -3,13 +3,19 @@
 
 #include <cstdint>
 
-extern volatile uint32_t can1_rx_frame_count;
-extern volatile uint32_t can2_rx_frame_count;
-extern volatile uint32_t can1_rx_fifo_full_count;
-extern volatile uint32_t can2_rx_fifo_full_count;
-extern volatile uint32_t can1_rx_fifo_lost_count;
-extern volatile uint32_t can2_rx_fifo_lost_count;
-extern volatile uint32_t can1_rx_read_error_count;
-extern volatile uint32_t can2_rx_read_error_count;
+namespace aim::ecat::diagnostics {
+    struct CanRxDiagnostics {
+        uint32_t can1_rx_frame_count{};
+        uint32_t can2_rx_frame_count{};
+        uint32_t can1_rx_fifo_full_count{};
+        uint32_t can2_rx_fifo_full_count{};
+        uint32_t can1_rx_fifo_lost_count{};
+        uint32_t can2_rx_fifo_lost_count{};
+        uint32_t can1_rx_read_error_count{};
+        uint32_t can2_rx_read_error_count{};
+    };
+
+    void get_can_rx_diagnostics(CanRxDiagnostics *snapshot);
+}
 
 #endif // ECATV2_AX58100_H750_UNIVERSAL_CAN_DIAGNOSTICS_HPP
